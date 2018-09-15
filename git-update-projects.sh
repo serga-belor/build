5f517ff8fecd969d9323e054557f779d4f1424eb
Sergey Belorusets <serga_belor_dev@outlook.com>
2018-09-15 17:20:34 -0400

    add git-update-projects.sh

--- git-update-projects.sh
+++ git-update-projects.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if git -C ./build pull \
-   && git -C ./http-server pull \
-   && git -C ./labirinth pull ; then
+if git -C ../build pull \
+   && git -C ../http-server pull \
+   && git -C ../labirinth pull ; then
 	echo git pull succedded
 else
 	echo !!! git pull failed
