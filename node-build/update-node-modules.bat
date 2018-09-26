0618538bfd24133390ff1e8bad38b7a8e5f2ee65
Sergey Belorusets <serga_belor_dev@outlook.com>
2018-09-26 23:39:12 +0300

    renme package.json.last with package.json.stamp

--- node-build/update-node-modules.bat
+++ node-build/update-node-modules.bat
@@ -1,5 +1,5 @@
-set PACKAGE_LAST=package.json.last
-if exist %PACKAGE_LAST% ( del /Q %PACKAGE_LAST% || (echo cannot delete %PACKAGE_LAST%& exit /B 1) )
+set PACKAGE_STAMP=package.json.stamp
+if exist %PACKAGE_STAMP% ( del /Q %PACKAGE_STAMP% || (echo cannot delete %PACKAGE_STAMP%& exit /B 1) )
 call npm update || (echo npm update failed& exit /B 1)
-copy package.json %PACKAGE_LAST% /Y || (echo cannot copy package.json to %PACKAGE_LAST%& exit /B 1)
+copy package.json %PACKAGE_STAMP% /Y || (echo cannot copy package.json to %PACKAGE_STAMP%& exit /B 1)
 @echo node modules updated& exit /B 0
